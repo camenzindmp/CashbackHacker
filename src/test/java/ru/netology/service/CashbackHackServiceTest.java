dfff675bf133782ad90ab4c5fbae3390eed086fa
Maksim <camenzindmp@gmail.com>
2020-08-24 20:58:11 +1000

    CashbackHackServiceTest added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,53 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void lessThan1k() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void evenly1k() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void moreThan1k() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1700;
+
+        int actual = service.remain(amount);
+        int expected = 300;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void roundedToRubles() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 937;
+
+        int actual = service.remain(amount);
+        int expected = 63;
+
+        assertEquals(actual, expected);
+    }
+}
+
