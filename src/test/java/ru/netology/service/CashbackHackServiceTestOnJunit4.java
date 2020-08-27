4a526fff8c107dd1d2565f98776f3cfba96c63a8
Maksim <camenzindmp@gmail.com>
2020-08-27 20:54:26 +1000

    JUnit4 plus JUnit5

--- src/test/java/ru/netology/service/CashbackHackServiceTestOnJunit4.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTestOnJunit4.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class CashbackHackServiceTest {
+public class CashbackHackServiceTestOnJunit4 {
 
     @Test
     public void lessThan1k() {
---
+++ src/test/java/ru/netology/service/CashbackHackServiceTestOnJunit5.java
@@ -0,0 +1,53 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CashbackHackServiceTestOnJunit5 {
+
+    @Test
+    public void lessThan1k() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void evenly1k() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void moreThan1k() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1700;
+
+        int actual = service.remain(amount);
+        int expected = 300;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void roundedToRubles() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 937;
+
+        int actual = service.remain(amount);
+        int expected = 63;
+
+        assertEquals(actual, expected);
+    }
+}
